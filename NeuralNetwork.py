43671d851d3516e3dbf2ef55eae88922d7594d1f
domi04 <98167794+domi04@users.noreply.github.com>
2022-05-28 17:00:44 +0000

    Completed Chapter 3

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,13 +1,26 @@
+from re import A
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
 
-inputs = [1, 2, 3, 2.5]
+class Layer_Dense:
 
-weights = [[0.2, 0.8, -0.5, 1],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+    def __init__(self,n_inputs, n_neurons):
+        # Initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-biases = [2, 3, 0.5]
+    def forward(self, inputs):
+        # Calculating output values from inputs, weights and biases
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-layer_outputs = np.dot(weights,inputs) + biases
 
-print(layer_outputs)
+X,y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
+
+dense1.forward(X)
+
+print(dense1.output[:5])
+
