39eecbd9584ab95088957ca0400d857952461f36
domi04 <98167794+domi04@users.noreply.github.com>
2022-06-20 08:13:43 +0000

    Implemented momentum on SGD Optimizer

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -134,25 +134,68 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs = self.dinputs / samples
 
 class Optimizer_SGD:
-    # initialize optimizer with a default learning rate of 1
-    def __init__(self, learning_rate=1., decay=0.):
+
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default for this optimizer
+    def __init__(self, learning_rate=1., decay=0., momentum=0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
-    
-    # call once before parameter updates 
+        self.momentum = momentum
+
+    # Call once before any parameter updates
     def pre_update_params(self):
         if self.decay:
-            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
-    
-    # update parameter
-    def update_params(self, layer):
-        layer.weights += -self.current_learning_rate * layer.dweights
-        layer.biases += -self.current_learning_rate * layer.dbiases
+            self.current_learning_rate = self.learning_rate * \
+                (1. / (1. + self.decay * self.iterations))
 
+    # Update parameters
+    def update_params(self, layer):
+
+        # If we use momentum
+        if self.momentum:
+
+            # If layer does not contain momentum arrays, create them
+            # filled with zeros
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                # If there is no momentum array for weights
+                # The array doesn't exist for biases yet either.
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            # Build weight updates with momentum - take previous
+            # updates multiplied by retain factor and update with
+            # current gradients
+            weight_updates = \
+                self.momentum * layer.weight_momentums - \
+                self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            # Build bias updates
+            bias_updates = \
+                self.momentum * layer.bias_momentums - \
+                self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        # Vanilla SGD updates (as before momentum update)
+        else:
+            weight_updates = -self.current_learning_rate * \
+                             layer.dweights
+            bias_updates = -self.current_learning_rate * \
+                           layer.dbiases
+
+        # Update weights and biases using either
+        # vanilla or momentum updates
+        layer.weights += weight_updates
+        layer.biases += bias_updates
+
+
+    # Call once after any parameter updates
     def post_update_params(self):
         self.iterations += 1
+
+
 
 X,y = spiral_data(samples=100, classes=3)
 
@@ -164,7 +207,7 @@ dense2 = Layer_Dense(64,3)
 
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
-optimizer = Optimizer_SGD(decay=1e-3)
+optimizer = Optimizer_SGD(decay=1e-3, momentum=0.5)
 
 # train loop
 for epoch in range(10001):
